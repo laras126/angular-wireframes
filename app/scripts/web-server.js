3dda99fc314c1cc3a27051349da02ad2687eaa73
laras126 <laras126@gmail.com>
2014-10-07 14:26:52 -0400

    debug heroku server

--- app/scripts/web-server.js
+++ app/scripts/web-server.js
@@ -1,6 +1,8 @@
+var port = process.env.PORT || 3000;
+
 function main(argv) {
   new HttpServer({
     'GET': createServlet(StaticServlet),
     'HEAD': createServlet(StaticServlet)
-  }).start(process.env.PORT || Number(argv[2]) || DEFAULT_PORT);
+  }).start(port);
 }
