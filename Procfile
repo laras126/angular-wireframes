bb6cc598fa0db799e76728717e8860cba1bf1c1a
laras126 <laras126@gmail.com>
2014-10-07 14:43:16 -0400

    continue heroku debug

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node app/scripts/web-server.js
+web: node web.js
--- app/scripts/web-server.js
+++
@@ -1,8 +0,0 @@
-var port = process.env.PORT || 3000;
-
-function main(argv) {
-  new HttpServer({
-    'GET': createServlet(StaticServlet),
-    'HEAD': createServlet(StaticServlet)
-  }).start(port);
-}
